664a24f8a8a83821f255381ebfbffdc01a0fdf40
trmtanjil <trmtanjil02@gmail.com>
2025-12-03 13:18:16 +0600

    Working with Environment-Based Configuration

--- level-2-moddule-11-server/src/config/index.ts
+++ level-2-moddule-11-server/src/config/index.ts
@@ -2,3 +2,10 @@ import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({path:path.join(process.cwd(),".env")})
+
+
+const config ={
+    env:process.env.NODE_ENV ? Number(process.env.NODE_ENV):5000,
+    port:process.env.PORT,
+}
+export default config;
--- level-2-moddule-11-server/src/server.ts
+++ level-2-moddule-11-server/src/server.ts
@@ -1,6 +1,7 @@
-import { config } from 'dotenv';
+ 
 import http, {IncomingMessage, Server, ServerResponse} from 'http';
 import path from 'path';
+import config from './config';
 
 const server:Server = http.createServer(
    ( req:IncomingMessage, res:ServerResponse)=>{
@@ -18,6 +19,6 @@ const server:Server = http.createServer(
    }
 )
 
-server.listen(5000,()=>{
-    console.log(`server running on port ${5000}`)
+server.listen(config.port,()=>{
+    console.log(`server running on port ${config.port}`)
 })
