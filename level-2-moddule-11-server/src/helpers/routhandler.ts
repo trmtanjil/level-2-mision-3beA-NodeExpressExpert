38a253aa4023f355e4bbd9bf62f5235590ce5bc4
trmtanjil <trmtanjil02@gmail.com>
2025-12-07 20:53:31 +0600

    Using Our Custom Route Handler

--- level-2-moddule-11-server/src/helpers/routhandler.ts
+++ level-2-moddule-11-server/src/helpers/routhandler.ts
@@ -1,11 +1,13 @@
-import { IncomingHttpHeaders, ServerResponse } from "http";
+import {  IncomingMessage, ServerResponse } from "http";
 
 
-type RouteHandler = (req:IncomingHttpHeaders, res:ServerResponse)=>void;
+export type RouteHandler = (req:IncomingMessage, res:ServerResponse)=>void;
 
-const routes : Map<string, Map<string, RouteHandler>>=new Map();
+export const routes : Map<string, Map<string, RouteHandler>>=new Map();
 
 function addRoutes(method: string, path:string,handler:RouteHandler){
     if(!routes.has(method)) routes.set(method, new Map());
     routes.get(method)!.set(path,handler)
 }
+
+export default addRoutes;
--- level-2-moddule-11-server/src/server.ts
+++ level-2-moddule-11-server/src/server.ts
@@ -2,10 +2,48 @@
 import http, {IncomingMessage, Server, ServerResponse} from 'http';
 import path from 'path';
 import config from './config';
+import addRoutes, { RouteHandler, routes } from './helpers/routhandler';
+
+addRoutes("GET", "/",(req,res)=>{
+    res.writeHead(200, {'content-type':'application/json'});
+    res.end(
+        JSON.stringify({
+            message:'Hello from node js with typescript...',
+            path:req.url,
+        })
+    )
+})
+
+
+
 
 const server:Server = http.createServer(
    ( req:IncomingMessage, res:ServerResponse)=>{
     console.log('server is running....');
+
+    const method =req.method?.toUpperCase()||'';
+
+    const path = req.url ||"";
+
+
+    const methodMap = routes.get(method)
+    const handler:RouteHandler|undefined = methodMap?.get(path)
+
+    if(handler){
+        handler(req,res);
+    }else{
+        res.writeHead(404, {'contect-type':'application/json'});
+        res.end(
+            JSON.stringify({
+                success:false,
+                message:'Route not found!!!',
+                path,
+            })
+        )
+    }
+
+
+
 
     //root route
     if(req.url=='/' && req.method =="GET"){
@@ -19,19 +57,19 @@ const server:Server = http.createServer(
     }
 
     //health route
-    if(req.url =="/api" && req.method=="GET"){
-        res.writeHead(200,{'content-type':'application/json'});
-        res.end(
-            JSON.stringify({
-                message:'health status ok',
-                path:req.url,
-            })
-        )
-    }
+    // if(req.url =="/api" && req.method=="GET"){
+    //     res.writeHead(200,{'content-type':'application/json'});
+    //     res.end(
+    //         JSON.stringify({
+    //             message:'health status ok',
+    //             path:req.url,
+    //         })
+    //     )
+    // }
 
 
     // post mathod
-    if(req.url=='/user' && req.method=='POST'){
+    // if(req.url=='/user' && req.method=='POST'){
         // const user ={
         //     id:1,
         //     name:'tanjil',
@@ -41,42 +79,42 @@ const server:Server = http.createServer(
         //     JSON.stringify(user)
         // )
 
-        let body = '';
+        // let body = '';
 
         //listen for data chunk
-        req.on('data',chunk=>{
-            body +=chunk.toString();
-        });
-        req.on('end',()=>{
-         try{
-               const parseBody = JSON.parse(body);
-            console.log(parseBody)
-            console.log('tanjil  vai catching chang')
-            res.end(JSON.stringify(parseBody))
-         }
-         catch(err:any){
-            console.log(err?.message)
-         }
-        })
-    }
+    //     req.on('data',chunk=>{
+    //         body +=chunk.toString();
+    //     });
+    //     req.on('end',()=>{
+    //      try{
+    //            const parseBody = JSON.parse(body);
+    //         console.log(parseBody)
+    //         console.log('tanjil  vai catching chang')
+    //         res.end(JSON.stringify(parseBody))
+    //      }
+    //      catch(err:any){
+    //         console.log(err?.message)
+    //      }
+    //     })
+    // }
 
-    if(req.url=='/api/mod'&& req.method=='POST'){
-        let body ='';
-        req.on('data',chunk=>{
-            body +=chunk.toString();
-        }) ;
-        req.on('end',()=>{
-            try{
-                const persBody=JSON.parse(body);
-                console.log(persBody)
-                console.log('tanjil is running api user')
-                res.end(JSON.stringify(persBody))
-            }
-            catch(err:any){
-                console.log(err.message)
-            }
-        })
-    }
+    // if(req.url=='/api/mod'&& req.method=='POST'){
+    //     let body ='';
+    //     req.on('data',chunk=>{
+    //         body +=chunk.toString();
+    //     }) ;
+    //     req.on('end',()=>{
+    //         try{
+    //             const persBody=JSON.parse(body);
+    //             console.log(persBody)
+    //             console.log('tanjil is running api user')
+    //             res.end(JSON.stringify(persBody))
+    //         }
+    //         catch(err:any){
+    //             console.log(err.message)
+    //         }
+    //     })
+    // }
    }
 )
 
