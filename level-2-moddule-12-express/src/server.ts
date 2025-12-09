65989e72401add9c2b1450dc86f811f72441d287
trmtanjil <trmtanjil02@gmail.com>
2025-12-09 16:55:10 +0600

    Connect with database using pool

--- level-2-moddule-12-express/src/server.ts
+++ level-2-moddule-12-express/src/server.ts
@@ -3,13 +3,31 @@ import {Pool}from "pg"
 const app = express();
 const port = 5000;
 
+//parser
+app.use(express.json());
+// app.use(express.urlencoded());
+
+//DB
 const pool = new Pool({
     connectionString:` postgresql://neondb_owner:npg_ly4T9HrAFnjo@ep-shiny-thunder-ah7yi6j6-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
 })
 
-//parser
-app.use(express.json());
-// app.use(express.urlencoded());
+const initDB = async()=>{
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(150) UNIQUE NOT NULL,
+        age INT,
+        phone CARCHAT(15),
+        address TEXT,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW(),
+        )
+        `)
+};
+initDB()
+
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello tanjil you are next level developer!')
