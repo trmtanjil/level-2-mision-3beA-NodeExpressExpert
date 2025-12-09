4fcb4690f90d450ce7ad5bccc220b89889a40cd4
trmtanjil <trmtanjil02@gmail.com>
2025-12-09 17:31:39 +0600

    Solve the previous error, add .env & connect with GitHub

--- level-2-moddule-12-express/src/server.ts
+++ level-2-moddule-12-express/src/server.ts
@@ -1,5 +1,9 @@
 import express, { Request, Response } from "express";
 import {Pool}from "pg"
+import dotenv from "dotenv"
+import path from "path"
+dotenv.config({path: path.join(process.cwd(), ".env")})
+
 const app = express();
 const port = 5000;
 
@@ -9,7 +13,7 @@ app.use(express.json());
 
 //DB
 const pool = new Pool({
-    connectionString:` postgresql://neondb_owner:npg_ly4T9HrAFnjo@ep-shiny-thunder-ah7yi6j6-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+    connectionString:`${process.env.COCCECTION_STR}`
 })
 
 const initDB = async()=>{
@@ -19,12 +23,25 @@ const initDB = async()=>{
         name VARCHAR(100) NOT NULL,
         email VARCHAR(150) UNIQUE NOT NULL,
         age INT,
-        phone CARCHAT(15),
+        phone VARCHAR(15),
         address TEXT,
         created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
         )
         `)
+
+      await pool.query(`
+    CREATE TABLE IF NOT EXISTS todos(
+        id SERIAL PRIMARY KEY,
+        user_id INT REFERENCES users(id) ON DELETE CASCADE,
+        title VARCHAR(200) NOT NULL,
+        description TEXT,
+        completed BOOLEAN DEFAULT false,
+        due_date DATE,
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+`)
 };
 initDB()
 
