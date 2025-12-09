3ce26eb38661a5c8e1b00c83d4eb58201caed570
trmtanjil <trmtanjil02@gmail.com>
2025-12-09 22:08:08 +0600

    app.get(/users/:id, async(req:Request, res:Response)=>{

--- level-2-moddule-12-express/src/server.ts
+++ level-2-moddule-12-express/src/server.ts
@@ -50,6 +50,8 @@ app.get('/', (req:Request, res:Response) => {
   res.send('Hello tanjil you are next level developer!')
 })
 
+//users CRUD
+
 app.post("/users",async (req:Request, res:Response)=>{
 const {name, email}= req.body;
 
@@ -67,17 +69,52 @@ res.status(201).json({
 }catch(err:any){
     res.status(500).json({
         success:false,
-        massege:err.message
+        message: err.message
     })
 }
 console.log(req.body)
-
-    res.status(201).json({
-        succes:true,
-        message:"API is working",
-    })
 })
+app.get("/users",async(req:Request, res:Response)=>{
+try{
+const result = await pool.query(`SELECT * FROM users`);
 
+res.status(200).json({
+    success:true,
+    message:"users retrived succesfully",
+    data:result.rows,
+})
+}catch(err:any){
+    res.status(5000).json({
+        success:false,
+        message:err.message,
+        details:err
+    })
+}
+})
+app.get("/users/:id", async(req:Request, res:Response)=>{
+    // console.log(req.params.id)
+    try{
+        const result = await pool.query(`SELECT * FROM users WHERE id = $1`,[req.params.id])
+
+        if(result.rows.length ===0){
+            res.status(404).json({
+                success:false,
+                message:'user not found'
+            }) 
+        }else{
+            res.status(200).json({
+                success:true,
+                message:'user fetched successfully',
+                data:result.rows[0]
+            })
+        }
+    }catch(err:any){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        })
+    }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
