08544abe167caf1390ffad7e756711087a850100
trmtanjil <trmtanjil02@gmail.com>
2025-12-09 15:50:51 +0600

    Create a simple server with express.js & typescript

---
+++ level-2-moddule-12-express/src/server.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express"
+const app = express();
+const port = 5000
+
+app.get('/', (req:Request, res:Response) => {
+  res.send('Hello tanjil you are next level developer!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
