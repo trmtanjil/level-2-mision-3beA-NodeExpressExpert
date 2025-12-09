af24e9cc380707192e144421411806a2368d493c
trmtanjil <trmtanjil02@gmail.com>
2025-12-09 16:22:43 +0600

    Installing Postgres as database

--- level-2-moddule-12-express/src/server.ts
+++ level-2-moddule-12-express/src/server.ts
@@ -1,10 +1,30 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
+import {Pool}from "pg"
 const app = express();
-const port = 5000
+const port = 5000;
+
+const pool = new Pool({
+    connectionString:` postgresql://neondb_owner:npg_ly4T9HrAFnjo@ep-shiny-thunder-ah7yi6j6-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require`
+})
+
+//parser
+app.use(express.json());
+// app.use(express.urlencoded());
 
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello tanjil you are next level developer!')
 })
+
+app.post("/", (req:Request, res:Response)=>{
+
+    console.log(req.body)
+
+    res.status(201).json({
+        succes:true,
+        message:"API is working",
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
