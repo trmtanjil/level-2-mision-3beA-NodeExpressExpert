de20676d0c64f40b3db5c3a34e9cab4b35cc5384
trmtanjil <trmtanjil02@gmail.com>
2025-12-10 12:48:37 +0600

    todos delete succesfully

--- level-2-moddule-12-express/src/server.ts
+++ level-2-moddule-12-express/src/server.ts
@@ -245,8 +245,32 @@ app.put("/todos/:id", logger, async(req:Request, res:Response) => {
         });
     }
 });
-// todo delete 
+// todos delete 
+app.delete("/todos/:id", async(req:Request, res:Response)=>{
+    // console.log(req.params.id)
+ 
+    try{
+        const result = await pool.query(`DELETE FROM todos WHERE id = $1`,[ req.params.id])
 
+        if(result.rowCount ==0){
+            res.status(404).json({
+                success:false,
+                message:'todos not found'
+            }) 
+        }else{
+            res.status(200).json({
+                success:true,
+                message:'todos delete successfully',
+                data:result.rows
+            })
+        }
+    }catch(err:any){
+        res.status(500).json({
+            success:false,
+            message:err.message,
+        })
+    }
+})
 
 app.use((req:Request, res:Response)=>{
     res.status(404).json({
