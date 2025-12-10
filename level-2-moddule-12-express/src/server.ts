f6380ade8f26886b22ecd583c4459725b2da1c15
trmtanjil <trmtanjil02@gmail.com>
2025-12-10 13:02:45 +0600

    Why Configuration Is Important?

--- level-2-moddule-12-express/src/server.ts
+++ level-2-moddule-12-express/src/server.ts
@@ -1,49 +1,19 @@
 import express, { NextFunction, request, Request, Response } from "express";
 import {Pool, Result}from "pg"
-import dotenv from "dotenv"
-import path from "path"
+
 import { userInfo } from "os";
-dotenv.config({path: path.join(process.cwd(), ".env")})
+import config from "./config";
+import initDB, { pool } from "./config/db";
+
 
 const app = express();
-const port = 5000;
+const port = config.port;
 
 //parser
 app.use(express.json());
 // app.use(express.urlencoded());
 
-//DB
-const pool = new Pool({
-    connectionString:`${process.env.COCCECTION_STR}`
-})
-
-const initDB = async()=>{
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `)
-
-      await pool.query(`
-    CREATE TABLE IF NOT EXISTS todos(
-        id SERIAL PRIMARY KEY,
-        user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(200) NOT NULL,
-        description TEXT,
-        completed BOOLEAN DEFAULT false,
-        due_date DATE,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-`)
-};
+//db
 initDB()
 
 const logger = (req:Request, res:Response, next:NextFunction)=>{
