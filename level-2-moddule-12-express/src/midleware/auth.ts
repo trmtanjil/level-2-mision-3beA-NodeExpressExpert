1ac524de1bb666d2b45d7ff0605985e0586b5f73
trmtanjil <trmtanjil02@gmail.com>
2025-12-11 12:27:43 +0600

    Auth Middleware & Declaring User in a Namespace

--- level-2-moddule-12-express/src/midleware/auth.ts
+++ level-2-moddule-12-express/src/midleware/auth.ts
@@ -1,18 +1,27 @@
  
 import { NextFunction, Request, Response } from "express"
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import config from "../config";
 
 
 const auth = ()=>{
-    return (req:Request, res:Response, next:NextFunction)=>{
-        const token = req.headers.authorization;
+    return async (req:Request, res:Response, next:NextFunction)=>{
+    try{
+            const token = req.headers.authorization;
         if(!token){
             return res.status(500).json({message:'You are not allowed!!'})
         }
         const decoded = jwt.verify(token, config.jwtSecret as string);
         console.log({decoded})
+        req.user =decoded as JwtPayload;
          return next();
+
+    }catch(err:any){
+        res.status(500).json({
+            success:false,
+            message:err.message
+        })
+    }
     }
 }
 export default auth;
