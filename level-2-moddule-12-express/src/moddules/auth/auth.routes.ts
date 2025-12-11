44cbffdfce3f2f8fb6ccd54fec75019e367c24d3
trmtanjil <trmtanjil02@gmail.com>
2025-12-11 08:50:13 +0600

    JWT Theory & Token Generation

--- level-2-moddule-12-express/src/moddules/auth/auth.routes.ts
+++ level-2-moddule-12-express/src/moddules/auth/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { authController } from "./auth.controller";
 
 const router = Router()
 
 
 
-
+router.post('/login', authController.loginUser)
 
 
 
--- level-2-moddule-12-express/src/moddules/auth/auth.service.ts
+++ level-2-moddule-12-express/src/moddules/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { pool } from "../../config/db"
 
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 
 const loginUser = async(email:string, password:string)=>{
     const result = await pool.query(`SELECT * FROM users WHERE email=$1`,[email])
@@ -17,5 +18,15 @@ const loginUser = async(email:string, password:string)=>{
         return false;
     }
 
-    
+    const secret = "KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
+    const token = jwt.sign({name:user.name, email:user.email},secret,{
+        expiresIn:'7d',
+    })
+    console.log({token})
+    return {token,user}
+
+}
+
+export const authServices={
+    loginUser
 }
